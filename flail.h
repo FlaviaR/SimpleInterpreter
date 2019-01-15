6a38c39fb280db74857b73d75eef1d3f8be8864e
Flavia Cavalcanti <flavia@cygnus.local>
2019-01-15 10:51:01 -0700

    Updated documentation. Stabilized byte array generation. Added Repeat and SetMode instructions.Can now accept both intensity and distance based parameters.

--- flail.h
+++ flail.h
@@ -6,15 +6,20 @@
 //
 //
 
+// If interpreter.h exists, define it
 #ifndef interpreter_h
 #define interpreter_h
-
-
 #endif /* interpreter_h */
+
+typedef int bool;
+#define true 1
+#define false 0
+
+enum mode {distance, intensity};
 
 typedef unsigned char byte;
 
-// List of instructions
+// List of instruction bytes
 typedef struct {
 	byte ascend;
 	byte forward;
@@ -26,9 +31,27 @@ typedef struct {
 	byte descend;
 	byte wait;
 	byte waitMili;
+    byte setMode;
 } Instructions;
 
+// List of instruction names
+typedef struct {
+    char* ascend;
+    char* forward;
+    char* backward;
+    char* left;
+    char* right;
+    char* rollL;
+    char* rollR;
+    char* descend;
+    char* wait;
+    char* waitMili;
+    char* setMode;
+    char* repeat;
+} InstructionList;
+
 // List of previously called instructions
+// This struct keeps track of how many times an instruction has been called
 // This struct is used to prevent conflicting instructions (e.g. left and right) from being
 // called at the same time.
 typedef struct {
@@ -45,15 +68,33 @@ typedef struct {
 
 // Association of specific bytes to instructions
 const Instructions inst = {
-	.ascend = 0x0,
-	.forward = 0x1,
-	.backward = 0x2,
-	.left = 0x3,
-	.right = 0x4,
-	.rollL = 0x5,
-	.rollR = 0x6,
-	.descend = 0x7,
-	.wait = 0x8,
-	.waitMili = 0x9
+	.ascend   = 0x1,
+	.forward  = 0x2,
+	.backward = 0x3,
+	.left     = 0x4,
+	.right    = 0x5,
+	.rollL    = 0x6,
+	.rollR    = 0x7,
+	.descend  = 0x8,
+	.wait     = 0x9,
+	.waitMili = 0xA,
+    .setMode = 0xB
 };
+
+// Association of specific instructions to their correct spelling
+const InstructionList instList = {
+    .ascend   = "Ascend",
+    .forward  = "Forward",
+    .backward = "Backward",
+    .left     = "Left",
+    .right    = "Right",
+    .rollL    = "RollLeft",
+    .rollR    = "RollRight",
+    .descend  = "Descend",
+    .wait     = "Wait",
+    .waitMili = "WaitMili",
+    .setMode  = "SetMode",
+    .repeat   = "Repeat"
+};
+
 
